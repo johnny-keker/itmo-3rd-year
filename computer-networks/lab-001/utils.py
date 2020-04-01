41fa2c50ab0a68f0e3ae49500cd39422924b9dcb
johnny-keker <giomail.iv@gmail.com>
2020-04-01 23:48:08 +0300

    [lab1] count bit intervals and build occurrence table scripts

--- computer-networks/lab-001/utils.py
+++ computer-networks/lab-001/utils.py
@@ -17,3 +17,41 @@ def longest_substing(s):
     return [max_0, '0']
   else:
     return [max_1, '1']
+
+def get_occurrence_counts(s, tokens = None):
+  tokens_provided = tokens != None
+  subs = []
+  if not tokens_provided:
+    subs += re.findall('0+', s)
+    subs += re.findall('1+', s)
+  else:
+    for token in tokens:
+      subs += re.findall(token, s)
+
+  res = {}
+  for token in subs:
+    key = token if tokens_provided else len(token)
+    if res.get(key):
+      res[key] += len(token)
+    else:
+      res[key] = len(token)
+  return res
+
+def build_occurrence_table(occurrence, certain_tokens = False):
+  print('\\begin{tabular}{| c | c | c | c |}')
+  print('\hline')
+  print(' Частота & Участки & Кол-во битовых интервалов & Отношение к $f_0$ \\\\')
+  print('\hline')
+  cur_index = 1
+
+  for key in occurrence:
+    row = f"$f_{{{cur_index}}}$ & "
+    row += key if certain_tokens else "1"*key + " и " + "0"*key
+    row += f" & {occurrence[key]} & "
+    row += f"$f_0 / "
+    row += str(len(key)) if certain_tokens else str(key)
+    row += "$ \\\\"
+    print(row)
+    cur_index += 1
+  print('\hline')
+  print('\\end{tabular}')
